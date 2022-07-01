946a33750de9644589dd043273777065f3f04819
YEJIN <yejon9858@gmail.com>
2022-07-02 03:25:33 +0900

    [FEAT] 메인 화면 xml, 뷰페이저 구현

--- GetIT/app/src/main/java/com/getit/getit/ui/main/home/HomeFragment.kt
+++ GetIT/app/src/main/java/com/getit/getit/ui/main/home/HomeFragment.kt
@@ -1,12 +1,45 @@
 package com.getit.getit.ui.main.home
 
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import androidx.viewpager2.widget.ViewPager2
+import com.example.flo.ui.main.home.BannerFragment
+import com.example.flo.ui.main.home.BannerVPAdapter
+import com.getit.getit.R
 import com.getit.getit.databinding.FragmentHomeBinding
 import com.getit.getit.ui.BaseFragment
+import me.relex.circleindicator.CircleIndicator3;
 
 
 class HomeFragment(): BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::inflate) {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        super.onCreateView(inflater, container, savedInstanceState)
+
+        Log.d("확인", binding.toString())
+
+        val bannerAdapter = BannerVPAdapter(this)
+        bannerAdapter.addFragment(BannerFragment(R.drawable.banner_image_1))
+        bannerAdapter.addFragment(BannerFragment(R.drawable.banner_image_2))
+        bannerAdapter.addFragment(BannerFragment(R.drawable.banner_image_3))
+
+        Log.d("확인", bannerAdapter.toString())
+        binding.homeBannerVp.adapter = bannerAdapter
+        binding.homeBannerVp.orientation = ViewPager2.ORIENTATION_HORIZONTAL
+
+        var indicator : CircleIndicator3 = binding.homeIndicator;
+        indicator.setViewPager(binding.homeBannerVp)
+        indicator.createIndicators(3, 1)
+
+        return binding.root
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
