08964e1fae1be35e2ea66b8a9157e4285fc0cc03
heekyoung2000 <heekyoung2000@naver.com>
2022-07-02 17:47:30 +0900

    <FEAT>Finish Viewpage

--- GetIT/app/src/main/java/com/getit/getit/ui/main/mypage/MypageFragment.kt
+++ GetIT/app/src/main/java/com/getit/getit/ui/main/mypage/MypageFragment.kt
@@ -1,6 +1,9 @@
 package com.getit.getit.ui.main.mypage
 
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.widget.Toolbar
+import com.getit.getit.databinding.ActivityWindowBinding
 import com.getit.getit.databinding.FragmentMypageBinding
 import com.getit.getit.ui.BaseFragment
 
@@ -10,4 +13,6 @@ class MypageFragment(): BaseFragment<FragmentMypageBinding>(FragmentMypageBindin
 
     override fun initAfterBinding() {
     }
+
+
 }
