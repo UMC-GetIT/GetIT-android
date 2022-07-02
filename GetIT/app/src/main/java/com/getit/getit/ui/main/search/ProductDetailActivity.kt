90965834db86472505391425398abbf30b75248e
DESKTOP-F989ETR\YujinChang <yujin1203713@gmail.com>
2022-07-02 12:29:03 +0900

    [FEAT] 상품 상세

---
+++ GetIT/app/src/main/java/com/getit/getit/ui/main/search/ProductDetailActivity.kt
@@ -0,0 +1,35 @@
+package com.getit.getit.ui.main.search
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.getit.getit.databinding.FragmentProductDetailBinding
+
+class ProductDetailActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        var binding = FragmentProductDetailBinding.inflate(layoutInflater)
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        var reviewDatas = ArrayList<Reviews>()
+        // 더미데이터
+        reviewDatas.apply {
+            add(Reviews("이름1", "리뷰내용리뷰내용리뷰내용리뷰내용리뷰내용리뷰내용리리리리릴리뷰뷰뷰뷰"))
+            add(Reviews("이름1", "리뷰내용리뷰내용리뷰내용리뷰내용리뷰내용리뷰내용"))
+            add(Reviews("이름1", "리뷰내용리뷰내용리뷰내용리뷰내용리뷰내용리뷰내용"))
+            add(Reviews("이름1", "리뷰내용리뷰내용리뷰내용리뷰내용리뷰내용리뷰내용"))
+            add(Reviews("이름1", "리뷰내용리뷰내용리뷰내용리뷰내용리뷰내용리뷰내용"))
+            add(Reviews("이름1", "리뷰내용리뷰내용리뷰내용리뷰내용리뷰내용리뷰내용"))
+            add(Reviews("이름1", "리뷰내용리뷰내용리뷰내용리뷰내용리뷰내용리뷰내용"))
+            add(Reviews("이름1", "리뷰내용리뷰내용리뷰내용리뷰내용리뷰내용리뷰내용"))
+            add(Reviews("이름1", "리뷰내용리뷰내용리뷰내용리뷰내용리뷰내용리뷰내용"))
+            add(Reviews("이름1", "리뷰내용리뷰내용리뷰내용리뷰내용리뷰내용리뷰내용"))
+            add(Reviews("이름1", "리뷰내용리뷰내용리뷰내용리뷰내용리뷰내용리뷰내용"))
+        }
+
+        val testRVAdatpter = ReviewRVAdatpter(reviewDatas)
+        binding.productDetailReviewRv.adapter = testRVAdatpter
+        binding.productDetailReviewRv.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+
+    }
+}
--- GetIT/app/src/main/java/com/getit/getit/ui/main/search/SearchFragment.kt
+++ GetIT/app/src/main/java/com/getit/getit/ui/main/search/SearchFragment.kt
@@ -1,12 +1,17 @@
 package com.getit.getit.ui.main.search
 
+import android.content.Intent
 import com.getit.getit.R
 import com.getit.getit.databinding.FragmentSearchBinding
 import com.getit.getit.ui.BaseFragment
+import com.getit.getit.ui.main.MainActivity
 
 class SearchFragment(): BaseFragment<FragmentSearchBinding>(FragmentSearchBinding::inflate) {
 
     override fun initAfterBinding() {
         binding.toolbar.root.title = "제품 조회"
+        binding.laptopIb.setOnClickListener {
+            startActivity(Intent(context, ProductDetailActivity::class.java))
+        }
     }
 }
