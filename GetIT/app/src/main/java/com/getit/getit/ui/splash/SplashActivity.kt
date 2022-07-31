47635a3c79af533d07bf0740efba8ea7b3194e4c
YEJIN <yejin9858@sookmyung.ac.kr>
2022-07-31 16:51:51 +0900

    [FEAT]  스플래시 화면

---
+++ GetIT/app/src/main/java/com/getit/getit/ui/splash/SplashActivity.kt
@@ -0,0 +1,34 @@
+package com.getit.getit.ui.splash
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import com.getit.getit.databinding.ActivityLoginBinding
+import com.getit.getit.databinding.ActivityMainBinding
+import com.getit.getit.databinding.ActivitySplashBinding
+import com.getit.getit.ui.BaseActivity
+import com.getit.getit.ui.login.LoginActivity
+import com.getit.getit.ui.main.MainActivity
+
+class SplashActivity : BaseActivity<ActivitySplashBinding>(ActivitySplashBinding::inflate)   {
+
+    override fun onCreate(savedInstanceState : Bundle?){
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        hideActionBar()
+
+        val handler = Handler(Looper.getMainLooper())
+
+        handler.postDelayed({
+            startActivity(Intent(this, LoginActivity::class.java))
+        }, 1000)
+
+    }
+
+    override fun initAfterBinding() {
+    }
+}
