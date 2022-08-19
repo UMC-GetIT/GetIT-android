7ba120c76c3b0c7892ec4a5ed926b989e3885fde
DESKTOP-F989ETR\YujinChang <yujin1203713@gmail.com>
2022-08-19 17:59:32 +0900

    [FEAT] 추천검색어 클릭 이벤트

--- GetIT/app/src/main/java/com/getit/getit/ui/main/searchproduct/RecommendRVAdapter.kt
+++ GetIT/app/src/main/java/com/getit/getit/ui/main/searchproduct/RecommendRVAdapter.kt
@@ -6,19 +6,20 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.getit.getit.databinding.ItemRecommendBinding
 
 class RecommendRVAdapter(val context: Context, val result : List<RecommendResult>) : RecyclerView.Adapter<RecommendRVAdapter.ViewHolder>(){
-//    interface MyItemClickListener{
-//        fun searchKeyword(keywordId: Int)
-//    }
-//
-//    private lateinit var mItemClickListener: MyItemClickListener
-//
-//    fun setMyItemClickListener(itemClickListener: MyItemClickListener){
-//        mItemClickListener = itemClickListener
-//    }
+    interface MyItemClickListener{
+        fun searchKeyword(keywordId: Int)
+    }
+
+    private lateinit var mItemClickListener: MyItemClickListener
+
+    fun setMyItemClickListener(itemClickListener: MyItemClickListener){
+        mItemClickListener = itemClickListener
+    }
 
     override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): RecommendRVAdapter.ViewHolder {
         val binding: ItemRecommendBinding = ItemRecommendBinding.inflate(LayoutInflater.from(viewGroup.context), viewGroup, false)
@@ -27,10 +28,10 @@ class RecommendRVAdapter(val context: Context, val result : List<RecommendResult
 
     override fun onBindViewHolder(holder: RecommendRVAdapter.ViewHolder, position: Int) {
         holder.keyword.text = result[position].keyword
-
-        holder.itemView.setOnClickListener {
-            val intent = Intent(holder.itemView?.context, SearchResultActivity::class.java)
-            ContextCompat.startActivity(holder.itemView.context, intent, null)
+        holder.binding.recommendKeyword.setOnClickListener {
+            val intent = Intent(context, SearchResultActivity::class.java)
+            intent.putExtra("keyword", result[position].keyword)
+            startActivity(holder.itemView.context, intent, null)
         }
     }
 
--- GetIT/app/src/main/java/com/getit/getit/ui/main/searchproduct/SearchProductRVAdapter.kt
+++ GetIT/app/src/main/java/com/getit/getit/ui/main/searchproduct/SearchProductRVAdapter.kt
@@ -30,7 +30,7 @@ class SearchProductRVAdapter(val context: Context, val result: List<SearchProduc
 
         val price = result[position].lprice
         val df = DecimalFormat("###,###")
-        val strPrice = df.format(price)
+        val strPrice = df.format(price) + "원"
         holder.price.text = strPrice
     }
 
