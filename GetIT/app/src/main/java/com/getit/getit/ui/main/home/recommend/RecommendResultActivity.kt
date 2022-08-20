3ed9e902306f2d38774a5b08320b900154c6119b
YEJIN <yejin9858@sookmyung.ac.kr>
2022-08-20 17:55:25 +0900

    [FEAT] 비교 뷰

---
+++ GetIT/app/src/main/java/com/getit/getit/ui/main/home/recommend/RecommendResultActivity.kt
@@ -0,0 +1,28 @@
+package com.getit.getit.ui.main.home.recommend
+
+import android.view.MenuItem
+import androidx.appcompat.widget.Toolbar
+import com.getit.getit.R
+import com.getit.getit.databinding.ActivityRecommendResultBinding
+import com.getit.getit.ui.BaseActivity
+
+class RecommendResultActivity  : BaseActivity<ActivityRecommendResultBinding>(ActivityRecommendResultBinding::inflate){
+    override fun initAfterBinding() {
+        val toolbar : Toolbar = binding.toolbar
+        toolbar.title = "비교 결과"
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true);
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_arrow_left);
+
+        setResultProducts()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId == android.R.id.home){
+            finish()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+}
