0bf41f3f7d24e7a7cd58fec635760f64cd4fd78a
heekyoung2000 <heekyoung2000@naver.com>
2022-08-20 17:45:04 +0900

    [UPDATE]mypage
    mypage

--- GetIT/app/src/main/java/com/getit/getit/ui/main/mypage/MypageFragment.kt
+++ GetIT/app/src/main/java/com/getit/getit/ui/main/mypage/MypageFragment.kt
@@ -1,10 +1,11 @@
 package com.getit.getit.ui.main.mypage
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import androidx.appcompat.app.AppCompatActivity
+import android.widget.TextView
 import androidx.appcompat.widget.Toolbar
 import androidx.core.view.children
 import com.getit.getit.R
@@ -16,6 +17,10 @@ import com.getit.getit.ui.main.mypage.review.ReviewProductAcitivity
 import com.getit.getit.ui.main.mypage.settings.ChangeProfileActivity
 import com.getit.getit.ui.main.mypage.settings.SettingActivity
 import com.getit.getit.utils.ApplicationClass
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
 
 
 class MypageFragment() : BaseFragment<FragmentMypageBinding>(FragmentMypageBinding::inflate), View.OnClickListener {
@@ -83,29 +88,37 @@ class MypageFragment() : BaseFragment<FragmentMypageBinding>(FragmentMypageBindi
 
         //엑세스 토큰이 필요하시면 이렇게 가져다 쓰시면 됩니다!
         //하지만 제가 자동으로 jwt가 헤더로 넘어가게끔 설정해놔서 필요할지는 모르겠어요
-        var accessToken = getJwt().toString();
+       var accessToken = getJwt().toString();
         Log.d("테스트", accessToken)
 
+
+
+
         //잠깐 주석처리 해놓겠습니다. 풀어서 써주세요!
-//        val mypageRetrofit =  ApplicationClass.retrofit.create(Service::class.java)
+        val name = view?.findViewById<TextView>(R.id.name)
+        val nickname = view?.findViewById<TextView>(R.id.nickname)
+        val mypageRetrofit = ApplicationClass.retrofit.create(MypageService::class.java)
 //        //Service class 이름 다른 이름과 겹칠 수 있기 때문에 MyPageService로 변경 부탁드려요!
-//        mypageRetrofit.getResponse().enqueue(object: Callback<UserInfo>{
-//            override fun onResponse(call: Call<UserInfo>, response: Response<UserInfo>) {
-//                Log.d("결과",response.body().toString())
-//                Log.d("결과",response.toString())
-//
-//                if (response.isSuccessful) {
-//                    val body = response.body()
-//                    body?.let {
-//                        Log.d("결과",body.toString())
-//                    }
-//                }
-//            }
-//
-//            override fun onFailure(call: Call<UserInfo>, t: Throwable) {
-//                Log.d("this is error", t.message.toString())
-//            }
-//        })
+       mypageRetrofit.getResponse().enqueue(object: Callback<UserInfo> {
+            override fun onResponse(call: Call<UserInfo>, response: Response<UserInfo>) {
+                Log.d("결과",response.body().toString())
+                Log.d("결과",response.toString())
+
+                if (response.isSuccessful) {
+                    val body = response.body()
+                    body?.let {
+                        Log.d("결과",body.toString())
+                        name?.text= body.result?.email.toString()
+                        nickname?.text = body.result?.nickname.toString()
+
+                    }
+                }
+            }
+
+            override fun onFailure(call: Call<UserInfo>, t: Throwable) {
+                Log.d("this is error", t.message.toString())
+            }
+        })
     }
 
     override fun initAfterBinding() {
--- GetIT/app/src/main/java/com/getit/getit/ui/main/mypage/UserInfo.kt
+++ GetIT/app/src/main/java/com/getit/getit/ui/main/mypage/UserInfo.kt
@@ -1,74 +1,65 @@
 package com.getit.getit.ui.main.mypage
 
 import com.google.gson.annotations.SerializedName
-data class ResponseData(
-    @SerializedName("code") val code : Int,
-    @SerializedName("isSuccess") val isSuccess:String,
-    @SerializedName("message") val message:String,
-    @SerializedName("result") val result: List<Result>
-) {
-    data class Result(
-        @SerializedName("userIdx") val userIdx: Int,
-        @SerializedName("email") val email: String,
-        @SerializedName("nickname")
-        val nickname: String,
-        //여기서 부터는 필요 없음
-        @SerializedName("birthday")
-        val birthday: Int,
+data class UserInfo(
+    @SerializedName("code") val code: Int,
+    @SerializedName("isSuccess") val isSuccess: String,
+    @SerializedName("message") val message: String,
+    @SerializedName("result") val result: Result?
+)
 
-        @SerializedName("job")
-        val job: String,
 
-        @SerializedName("status")
-        val status: String,
+data class Result(
+    @SerializedName("userIdx") val userIdx: Int,
+    @SerializedName("email") val email: String,
+    @SerializedName("nickname") val nickname: String,
+    @SerializedName("role")
+    val role: String,
+    @SerializedName("likeProduct")
+    val likeProduct:List<likeProduct>,
 
-        @SerializedName("role")
-        val role: String,
-        @SerializedName("likeProduct")
-        val likeProduct:List<likeProduct>,
+    @SerializedName("review")
+    val review:List<Review>
 
-        @SerializedName("review")
-        val review:List<Review>
+    //@SerializedName("image")
+    //val image:String
+)
 
-        //@SerializedName("image")
-        //val image:String
-    )
+data class likeProduct(
+    @SerializedName("productIdx")
+    val productIdx: Int,
 
-    data class likeProduct(
-        @SerializedName("productIdx")
-        val productIdx: Int,
+    @SerializedName("image")
+    val image: String,
 
-        @SerializedName("image")
-        val image: String,
+    @SerializedName("brand")
+    val brand: String,
 
-        @SerializedName("brand")
-        val brand: String,
+    @SerializedName("name")
+    val name: String,
 
-        @SerializedName("name")
-        val name: String,
-
-        @SerializedName("productUrl")
-        val productUrl: String
+    @SerializedName("productUrl")
+    val productUrl: String
 
 //brand이름이랑 name 이름이랑 따로 주는지 아니면 같이 주는지
-        //productId, name이랑 뭐가 다른지
-        //lowestprice가 필요한지
+    //productId, name이랑 뭐가 다른지
+    //lowestprice가 필요한지
+
+)
+
+data class Review(
+    @SerializedName("userIdx")
+    val userIdx: Int,
+
+    @SerializedName("review")
+    val review: String,
+
+    @SerializedName("reviewImgUrl")
+    val reviewImgUrl: String,
+
+    @SerializedName("image")
+    val image: String,
+
 
     )
 
-    data class Review(
-        @SerializedName("userIdx")
-        val userIdx: Int,
-
-        @SerializedName("review")
-        val review: String,
-
-        @SerializedName("reviewImgUrl")
-        val reviewImgUrl: String,
-
-        @SerializedName("image")
-        val image: String,
-
-
-        )
-}
