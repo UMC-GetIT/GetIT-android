084e5b0754ad34e2a128f6ec0231ea02aca72655
DESKTOP-F989ETR\YujinChang <yujin1203713@gmail.com>
2022-08-22 10:48:54 +0900

    [FEAT] 웹페이지 연결

--- GetIT/app/src/main/java/com/getit/getit/ui/main/category/detail/ProductDetailActivity.kt
+++ GetIT/app/src/main/java/com/getit/getit/ui/main/category/detail/ProductDetailActivity.kt
@@ -1,5 +1,7 @@
 package com.getit.getit.ui.main.category.detail
 
+import android.content.Intent
+import android.net.Uri
 import android.text.Editable
 import android.text.TextWatcher
 import android.util.Log
@@ -15,7 +17,6 @@ import com.getit.getit.ui.main.category.Reviews
 import com.google.gson.Gson
 
 class ProductDetailActivity: BaseActivity<ActivityProductDetailBinding>(ActivityProductDetailBinding::inflate), ProductDetailView {
-    private var gson: Gson = Gson()
     private lateinit var productId: String
 
     override fun initAfterBinding() {
@@ -36,23 +37,10 @@ class ProductDetailActivity: BaseActivity<ActivityProductDetailBinding>(Activity
 
         getProductDetail(productId)
 
-//        val intent = intent
-//        val productJson = intent.getStringExtra("product")
-//        val product = gson.fromJson(productJson, Products::class.java)
-//        setInit(product)
-
-
-        // 리뷰 더미데이터
-//        reviewDatas.apply {
-//            add(Reviews(R.drawable.dummy_review_img ,"재니퍼", "역대급 성능입니다! 웬만한 작업으로는 팬도 안 돌아가서 소음도 느껴지지 않네요. 강추합니다! 역대급 성능입니다! 웬만한 작업으로는 팬도 안 돌아가서 소음도 느껴지지 않네요. 강추합니다!"))
-//            add(Reviews(R.drawable.dummy_review_img ,"지온", "저는 포토샵이나 파이널컷 프로 정도 살짝 건드리는 수준인데 약간 오버스펙인 면이 없지 않아있지만 만족합니다."))
-//            add(Reviews(R.drawable.dummy_review_img ,"설기", "상자 뜯는 재미가 있어서 좋았습니다. 성능이 상상을 초월하네요 기대하고 구매했는데 잘 사용하겠습니다."))
-//        }
-
-        var reviewDatas = ArrayList<Reviews>()
-        val reviewRVAdatpter = ReviewRVAdatpter(reviewDatas)
-        binding.productDetailReviewRv.adapter = reviewRVAdatpter
-        binding.productDetailReviewRv.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+//        var reviewDatas = ArrayList<Reviews>()
+//        val reviewRVAdatpter = ReviewRVAdatpter(reviewDatas)
+//        binding.productDetailReviewRv.adapter = reviewRVAdatpter
+//        binding.productDetailReviewRv.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
 
         binding.productDetailLikeBtnOffIb.setOnClickListener {
             likeOn()
@@ -94,21 +82,16 @@ class ProductDetailActivity: BaseActivity<ActivityProductDetailBinding>(Activity
         binding.productDetailLikeBtnOffIb.visibility = View.VISIBLE
     }
 
-    private fun setInit() {
-//        binding.productDetailImgIv.setImageResource(product.coverImg!!)
-//        binding.productDetailProductNameTv.text = product.name
-        // 천 단위 콤마 넣기
-//        var price = product.price
-//        val dec = DecimalFormat("#,###")
-//        binding.productDetailProductPriceTv.text = dec.format(price).toString() + "원"
-    }
-
     override fun onGetProductDetailLoading() {
         Log.d("PRODUCT-DETAIL", "로딩중")
     }
 
     override fun onGetProductDetailSuccess(Code: Int, result: ProductDetailResult) {
         binding.productDetailProductNameTv.text = result.name
+        binding.productDetailPurchaseBtn.setOnClickListener {
+            var purchaseLink = result.link
+            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(purchaseLink)))
+        }
 
     }
 
